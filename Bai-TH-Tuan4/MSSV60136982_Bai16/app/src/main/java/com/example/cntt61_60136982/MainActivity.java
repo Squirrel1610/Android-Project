f3ad1fb612fc082e37d3b3cc2fa9f8b4361a6140
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-03-27 13:32:38 +0700

    finish bai 16

---
+++ Bai-TH-Tuan4/MSSV60136982_Bai16/app/src/main/java/com/example/cntt61_60136982/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.cntt61_60136982;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv_show = (TextView) findViewById(R.id.tv_pos_val);
+        String[] listName = getResources().getStringArray(R.array.list_name);
+        ListView list_view = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter;
+
+
+        adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, listName
+        );
+
+        list_view.setAdapter(adapter);
+
+       list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+               String val = (String) list_view.getItemAtPosition(pos);
+               tv_show.setText("position: " + pos + " ; value = " + val);
+           }
+       });
+    }
+
+
+}
