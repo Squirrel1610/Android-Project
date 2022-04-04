1a619c763a1ba64593d89ddc0f1d6b137279d58d
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-04-04 23:24:00 +0700

    finish login

--- Bai-TH-29-3/LoginApp/app/src/main/java/com/example/loginapp/HomeActivity.java
+++ Bai-TH-29-3/LoginApp/app/src/main/java/com/example/loginapp/HomeActivity.java
@@ -1,12 +1,42 @@
 package com.example.loginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent iManHinhChinh = getIntent();
+        TextView textView_Username = (TextView) findViewById(R.id.tv_username1);
+        textView_Username.setText(iManHinhChinh.getStringExtra("Username"));
+        Button nut_thoat = (Button) findViewById(R.id.btn_quitapp);
+        Button nut_dangxuat = (Button) findViewById(R.id.btn_logout);
+
+        nut_dangxuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iQuayLai = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(iQuayLai);
+            }
+        });
+
+        nut_thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+
     }
 }
--- Bai-TH-29-3/LoginApp/app/src/main/java/com/example/loginapp/LoginActivity.java
+++ Bai-TH-29-3/LoginApp/app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -13,12 +16,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button nut_xacnhan = (Button) findViewById(R.id.btn_ok);
+        EditText editText_username = (EditText) findViewById(R.id.ed_username);
+        EditText editText_password = (EditText) findViewById(R.id.ed_password);
+        EditText editText_email = (EditText) findViewById(R.id.ed_email);
+
 
         nut_xacnhan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent iManHinhChinh = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(iManHinhChinh);
+                String username = editText_username.getText().toString();
+                String email = editText_email.getText().toString();
+                String password = editText_password.getText().toString();
+                if(username.equals("Duythinh") && email.equals("Slthinhtu2@gmail.com") && password.equals("Dthinh1610")){
+                    Intent iManHinhChinh = new Intent(LoginActivity.this, HomeActivity.class);
+                    iManHinhChinh.putExtra("Username", username);
+                    startActivity(iManHinhChinh);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Thông tin bạn nhập không đúng, vui lòng nhập lại!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
